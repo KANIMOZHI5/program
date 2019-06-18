4c8e3c53980327109b7a8dd9e6422115ff58083b
KANIMOZHI5 <51222585+KANIMOZHI5@users.noreply.github.com>
2019-06-18 16:28:29 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,7 +1,7 @@
 z=input()
-if z="a"or"A" or"e"or"E" or"i"or"I"or"o"or"O"or"u"or"U":
+if z=="a"or"A" or"e"or"E" or"i"or"I"or"o"or"O"or"u"or"U":
   print("Vowel")
-elif z="b"or"c"or"d"or"g"or"f"or"h"or"j"or"k"or"l"or"m"or"n"or"p"or"q"or"r"or"s"or"t"or"v"or"w"or"x"or"y"or"z"or"B"or"C"or"D"or"F"or"G"or"H"or"J"or"K"or"L"or"M"or"N"or"P"or"Q"or"R"or"S"or"T"or"V"or"W"or"X"or"Y"or"Z":
+elif z=="b"or"c"or"d"or"g"or"f"or"h"or"j"or"k"or"l"or"m"or"n"or"p"or"q"or"r"or"s"or"t"or"v"or"w"or"x"or"y"or"z"or"B"or"C"or"D"or"F"or"G"or"H"or"J"or"K"or"L"or"M"or"N"or"P"or"Q"or"R"or"S"or"T"or"V"or"W"or"X"or"Y"or"Z":
   print("Consonant")
 else:
   print("invalid"
