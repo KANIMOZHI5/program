63d05fdc514ee2a3386302b2d53111b997d64a7c
KANIMOZHI5 <51222585+KANIMOZHI5@users.noreply.github.com>
2019-06-18 16:21:47 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,5 +1,5 @@
 z=input()
-if z="a"or"A" orz="e"or"E" or z="i" or"I" or z="o" or"O" or z="u" or "U":
+if z="a"or"A" or"e"or"E" or"i"or"I"or"o"or"O"or"u"or"U":
   print("Vowel")
 elif z="b"or"c"or"d"or"g"or"f"or"h"or"j"or"k"or"l"or"m"or"n"or"p"or"q"or"r"or"s"or"t"or"v"or"w"or"x"or"y"or"z":
   print("Consonants")
