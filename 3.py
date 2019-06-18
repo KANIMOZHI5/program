548f6ae09f49d4dd7001bb83c483048b4dca11e9
KANIMOZHI5 <51222585+KANIMOZHI5@users.noreply.github.com>
2019-06-18 16:30:15 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -4,4 +4,4 @@ if z=="a"or"A" or"e"or"E" or"i"or"I"or"o"or"O"or"u"or"U":
 elif z=="b"or"c"or"d"or"g"or"f"or"h"or"j"or"k"or"l"or"m"or"n"or"p"or"q"or"r"or"s"or"t"or"v"or"w"or"x"or"y"or"z"or"B"or"C"or"D"or"F"or"G"or"H"or"J"or"K"or"L"or"M"or"N"or"P"or"Q"or"R"or"S"or"T"or"V"or"W"or"X"or"Y"or"Z":
   print("Consonant")
 else:
-  print("invalid"
+  print("invalid")
