8bbdcf15266778d86f8d45d0a929ae8be665d334
KANIMOZHI5 <51222585+KANIMOZHI5@users.noreply.github.com>
2019-06-18 16:18:42 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,7 @@
+z=input()
+if z="a"or"A" orz="e"or"E" or z="i" or"I" or z="o" or"O" or z="u" or "U":
+  print("Vowel")
+elif z="b"or"c"or"d"or"g"or"f"or"h"or"j"or"k"or"l"or"m"or"n"or"p"or"q"or"r"or"s"or"t"or"v"or"w"or"x"or"y"or"z":
+  print("Consonants")
+else:
+  print("invalid"
